ceb4b2da63e0af9f010f42d9004b75a758e90aac
Jesse Cummins <me@jessecummins.com>
2019-09-02 19:55:28 -0400

    Add size for temp robot pic

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = () => {
     return (
         <div>
-            <img src="https://robohash.org/temp_robot" alt="photo of robot friend" />
+            <img src="https://robohash.org/temp_robot?size=200x200" alt="photo of robot friend" />
             <div>
                 <h2>Alex Friend</h2>
                 <p>alexfriend@email.com</p>
