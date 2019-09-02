1c055a8b17e275bb288c87a7c18539f357b3d64b
Jesse Cummins <me@jessecummins.com>
2019-09-02 19:47:13 -0400

    Add Card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img src="photo of robot friend" alt="" />
+            <div>
+                <h2>Alex Friend</h2>
+                <p>alexfriend@email.com</p>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
