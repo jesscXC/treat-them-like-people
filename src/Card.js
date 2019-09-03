42cabee38f8f7108e40f0c9360b1bd4c0590c0ab
Jesse Cummins <me@jessecummins.com>
2019-09-02 20:01:48 -0400

    Add more styling to Card component

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = () => {
     return (
-        <div className="bg-light-green dib br3 pa3 ma2 grow">
+        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
             <img src="https://robohash.org/temp_robot?size=200x200" alt="photo of robot friend" />
             <div>
                 <h2>Alex Friend</h2>
