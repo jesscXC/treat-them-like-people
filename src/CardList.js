0c4276e61464e52b8ece82d693c779161b2280f4
Jesse Cummins <me@jessecummins.com>
2019-09-02 22:42:37 -0400

    Use better key for Card in CardList component

--- src/CardList.js
+++ src/CardList.js
@@ -5,7 +5,7 @@ const CardList = ({ robots }) => {
     const cardsArray = robots.map((user, i) => {
         return(
             <Card
-                key={i}
+                key={robots[i].id}
                 id={robots[i].id}
                 name={robots[i].name}
                 email={robots[i].email}
