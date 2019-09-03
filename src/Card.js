6ff99b6187b909eee9414c74b4f6814c4efdf6ac
Jesse Cummins <me@jessecummins.com>
2019-09-02 22:03:56 -0400

    Use destructuring in Card component for robot contact info

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const Card = (props) => {
+    const { name, email, id } = props
     return (
         // Using tachyons library for fast CSS styling.
         <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img src={`https://robohash.org/${props.id}?size=200x200`} alt="robot friend" />
+            <img src={`https://robohash.org/${id}?size=200x200`} alt="robot friend" />
             <div>
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
