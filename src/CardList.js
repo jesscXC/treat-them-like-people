b71dd91f850686143de557b77200aed36039aec2
Jesse Cummins <me@jessecummins.com>
2019-09-02 22:37:31 -0400

    Improve indentation formatting in CardList component

--- src/CardList.js
+++ src/CardList.js
@@ -2,19 +2,19 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-const cardsArray = robots.map((user, i) => {
+    const cardsArray = robots.map((user, i) => {
+        return(
+            <Card
+                id={robots[i].id}
+                name={robots[i].name}
+                email={robots[i].email}
+            />
+    )});
     return(
-        <Card
-            id={robots[i].id}
-            name={robots[i].name}
-            email={robots[i].email}
-        />
-        )});
-return(
-    <div>
-        {cardsArray}
-    </div>
-);
+        <div>
+            {cardsArray}
+        </div>
+    );
 };
 
 export default CardList
