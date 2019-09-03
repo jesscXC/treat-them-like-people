afed652a058c352ecd08c8267e786b33ca9aa802
Jesse Cummins <me@jessecummins.com>
2019-09-02 20:09:54 -0400

    Update styling; Fix alt info for img tag; Add some default contact info for some robot friends

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = () => {
     return (
         <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img src="https://robohash.org/temp_robot?size=200x200" alt="photo of robot friend" />
+            <img src="https://robohash.org/temp_robot?size=200x200" alt="robot friend" />
             <div>
                 <h2>Alex Friend</h2>
                 <p>alexfriend@email.com</p>
---
+++ src/robots.js
@@ -0,0 +1,14 @@
+// Default test data for our robot friends.
+
+export const = robots [
+    {
+        id: 1,
+        name: "Sam Robot",
+        email: "friendly_person@email.com"
+    },
+    {
+        id: 2,
+        name: "Sky Robot",
+        email: "i_love_helping@email.com"
+    }
+];
