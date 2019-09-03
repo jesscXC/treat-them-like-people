23770cbb2814eb77f82575fe3fec144f8b931d77
Jesse Cummins <me@jessecummins.com>
2019-09-03 15:24:05 -0400

    Add SearchBox component; Standardize formatting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import SearchBox from './SearchBox';
 import CardList from './CardList';
 import { robots } from './robots.js';
 
@@ -6,9 +7,10 @@ const App = () => {
     return (
         <div>
             <h1>Treat Them Like People</h1>
+            <SearchBox />
             <CardList robots={robots} />
         </div>
-        )
-}
+    );
+};
 
 export default App;
---
+++ src/SearchBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchBox = () => {
+    return (
+        <div>
+        <input type="search" placeholder="search robot friends" />
+        </div>
+    );
+};
+
+export default SearchBox
