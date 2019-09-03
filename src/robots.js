b369c01194d787559a2d7c3d145146d36843f64a
Jesse Cummins <me@jessecummins.com>
2019-09-02 22:02:23 -0400

    Add robot friends and their contact info

--- src/robots.js
+++ src/robots.js
@@ -18,5 +18,35 @@ export const robots = [
         name: "Sky Robot",
         username: "i_love_helping",
         email: "i_love_helping@email.com"
-    }
+    },
+    {
+        id: 3,
+        name: "Link Friend",
+        username: "link_the_world",
+        email: "link_the_world@email.com"
+    },
+    {
+        id: 4,
+        name: "Page Technicality",
+        username: "technically_true",
+        email: "technically_true@email.com"
+    },
+    {
+        id: 5,
+        name: "Arc Language",
+        username: "list_processor",
+        email: "list_processor@email.com"
+    },
+    {
+        id: 6,
+        name: "Noble Attribute",
+        username: "style_helps",
+        email: "style_helps@email.com"
+    },
+    {
+        id: 7,
+        name: "Ada Number",
+        username: "original_programmin",
+        email: "original_programming@email.com"
+    },
 ];
