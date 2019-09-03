82b1cc389a3cb358ead4093c6ad983bf23c63ec0
Jesse Cummins <me@jessecummins.com>
2019-09-02 22:40:43 -0400

    Add key to each Card in CardList component

--- src/CardList.js
+++ src/CardList.js
@@ -5,6 +5,7 @@ const CardList = ({ robots }) => {
     const cardsArray = robots.map((user, i) => {
         return(
             <Card
+                key={i}
                 id={robots[i].id}
                 name={robots[i].name}
                 email={robots[i].email}
