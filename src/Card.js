5de17841a4615c0a93a552a3283eb1a2ae556017
Jesse Cummins <me@jessecummins.com>
2019-09-02 21:53:16 -0400

    Change CSS styling (center robot contact info)

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = (props) => {
     return (
         // Using tachyons library for fast CSS styling.
-        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
             <img src="https://robohash.org/temp_robot?size=200x200" alt="robot friend" />
             <div>
                 <h2>{props.name}</h2>
