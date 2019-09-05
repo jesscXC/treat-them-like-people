3e0e92173503ce145d654239eaf25b27e538ded7
Jesse Cummins <me@jessecummins.com>
2019-09-05 14:29:55 -0400

    Change App to a class; Add state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SearchBox from './SearchBox';
 import CardList from './CardList';
 import { robots } from './robots.js';
 
-const App = () => {
-    return (
+const state = {
+    robots: robots,
+    searchfield: ""
+};
+
+class App extends Component {
+    render() {
+        return (
         <div className="tc">
             <h1>Treat Them Like People</h1>
             <SearchBox />
             <CardList robots={robots} />
         </div>
-    );
+        );
+    };
 };
 
 export default App;
