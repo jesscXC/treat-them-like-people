6cac073d758431eb3c8f48a29da65dd72276e278
Jesse Cummins <me@jessecummins.com>
2019-09-11 16:26:35 -0400

    Apply JsPrettier to App.js

--- src/App.js
+++ src/App.js
@@ -5,35 +5,36 @@ import { robots } from './robots.js';
 import './App.css';
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchtext: ''
-        }
-    }
-
-    componentDidMount() {
-        this.setState({ robots: robots });
-    }
-
-    onSearchChange = (event) => {
-        this.setState({ searchtext: event.target.value });
-    }
-
-    render() {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchtext.toLowerCase());
-        })
-        return (
-            <div className="tc">
-                <h1 className="f1">Treat Them Like People</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            </div>
-            );
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchtext: ''
     };
-};
+  }
+
+  componentDidMount() {
+    this.setState({ robots: robots });
+  }
+
+  onSearchChange = event => {
+    this.setState({ searchtext: event.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchtext.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1 className="f1">Treat Them Like People</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
 
 export default App;
-
