132a8be0d5a39b68170eafb5ae957d8404ec6945
Jesse Cummins <me@jessecummins.com>
2019-09-11 16:27:48 -0400

    Apply JsPrettier to Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const Card = ({ name, email, id }) => {
-    return (
-        // Using tachyons library for fast CSS styling.
-        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img src={`https://robohash.org/${id}?size=200x200`} alt="robot friend" />
-            <div>
-                <h2>{name}</h2>
-                <p>{email}</p>
-            </div>
-        </div>
-    );
+  return (
+    // Using tachyons library for fast CSS styling.
+    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img src={`https://robohash.org/${id}?size=200x200`} alt="robot friend" />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
