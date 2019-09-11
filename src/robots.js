790fd3207e3f259c341aa2fccd74a3b2da2cc42f
Jesse Cummins <me@jessecummins.com>
2019-09-11 16:31:20 -0400

    Apply JsPrettier to robots.js

--- src/robots.js
+++ src/robots.js
@@ -1,51 +1,51 @@
 // Default test data for our robot friends.
 export const robots = [
-    {
-        id: 0,
-        name: 'Alex Friend',
-        username: 'alex_friend',
-        email: 'alex_friend@email.com'
-    },
-    {
-        id: 1,
-        name: 'Sam Robot',
-        username: 'friendly_person',
-        email: 'friendly_person@email.com'
-    },
-    {
-        id: 2,
-        name: 'Sky Robot',
-        username: 'i_love_helping',
-        email: 'i_love_helping@email.com'
-    },
-    {
-        id: 3,
-        name: 'Link Friend',
-        username: 'link_the_world',
-        email: 'link_the_world@email.com'
-    },
-    {
-        id: 4,
-        name: 'Page Technicality',
-        username: 'technically_true',
-        email: 'technically_true@email.com'
-    },
-    {
-        id: 5,
-        name: 'Arc Language',
-        username: 'list_processor',
-        email: 'list_processor@email.com'
-    },
-    {
-        id: 6,
-        name: 'Noble Attribute',
-        username: 'style_helps',
-        email: 'style_helps@email.com'
-    },
-    {
-        id: 7,
-        name: 'Ada Number',
-        username: 'original_programmin',
-        email: 'original_programming@email.com'
-    }
+  {
+    id: 0,
+    name: 'Alex Friend',
+    username: 'alex_friend',
+    email: 'alex_friend@email.com'
+  },
+  {
+    id: 1,
+    name: 'Sam Robot',
+    username: 'friendly_person',
+    email: 'friendly_person@email.com'
+  },
+  {
+    id: 2,
+    name: 'Sky Robot',
+    username: 'i_love_helping',
+    email: 'i_love_helping@email.com'
+  },
+  {
+    id: 3,
+    name: 'Link Friend',
+    username: 'link_the_world',
+    email: 'link_the_world@email.com'
+  },
+  {
+    id: 4,
+    name: 'Page Technicality',
+    username: 'technically_true',
+    email: 'technically_true@email.com'
+  },
+  {
+    id: 5,
+    name: 'Arc Language',
+    username: 'list_processor',
+    email: 'list_processor@email.com'
+  },
+  {
+    id: 6,
+    name: 'Noble Attribute',
+    username: 'style_helps',
+    email: 'style_helps@email.com'
+  },
+  {
+    id: 7,
+    name: 'Ada Number',
+    username: 'original_programmin',
+    email: 'original_programming@email.com'
+  }
 ];
