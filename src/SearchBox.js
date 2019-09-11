bd82255ab6e85bc70526b84b0719392381fc014d
Jesse Cummins <me@jessecummins.com>
2019-09-11 16:32:17 -0400

    Apply JsPrettier to SearchBox.js

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const SearchBox = ({ searchfield, searchChange }) => {
-    return (
-        <div className="pa2">
-            <input
-                type="search"
-                placeholder="search your robot friends"
-                className="pa3 ba b--green bg-lightest-blue"
-                onChange={searchChange}
-            />
-        </div>
-    );
+  return (
+    <div className="pa2">
+      <input
+        type="search"
+        placeholder="search your robot friends"
+        className="pa3 ba b--green bg-lightest-blue"
+        onChange={searchChange}
+      />
+    </div>
+  );
 };
 
-export default SearchBox
+export default SearchBox;
