af34f91c7d47d9b9dd6a3e77df4bc6f3fe6d1000
Jesse Cummins <me@jessecummins.com>
2019-09-12 16:08:23 -0400

    Change IDs for robots.js to use UUIDs

--- src/robots.js
+++ src/robots.js
@@ -1,49 +1,49 @@
 // Default test data for our robot friends.
 export const robots = [
   {
-    id: 0,
+    id: 'e4c2af75-28d0-4e7c-9bea-25406204d5f0',
     name: 'Alex Friend',
     username: 'alex_friend',
     email: 'alex_friend@email.com'
   },
   {
-    id: 1,
+    id: '73a49e9a-1bb5-41fc-84da-5474ed3f05aa',
     name: 'Sam Robot',
     username: 'friendly_person',
     email: 'friendly_person@email.com'
   },
   {
-    id: 2,
+    id: 'b51768d9-58fa-4dec-9925-168412594347',
     name: 'Sky Robot',
     username: 'i_love_helping',
     email: 'i_love_helping@email.com'
   },
   {
-    id: 3,
+    id: '3845efa9-a8c6-4c5a-ae8c-8723c2da1ecf',
     name: 'Link Friend',
     username: 'link_the_world',
     email: 'link_the_world@email.com'
   },
   {
-    id: 4,
+    id: 'e85b1ad1-e1ba-4880-aee0-bfab20212c06',
     name: 'Page Technicality',
     username: 'technically_true',
     email: 'technically_true@email.com'
   },
   {
-    id: 5,
+    id: '59ec51ce-6a6d-4dcb-b38a-43f28b850830',
     name: 'Arc Language',
     username: 'list_processor',
     email: 'list_processor@email.com'
   },
   {
-    id: 6,
+    id: 'a2398ccd-ad58-4f11-a73a-29ddf3ff31c0',
     name: 'Noble Attribute',
     username: 'style_helps',
     email: 'style_helps@email.com'
   },
   {
-    id: 7,
+    id: 'e6d621f6-1fe4-43e0-97af-3a377e06fabf',
     name: 'Ada Number',
     username: 'original_programmin',
     email: 'original_programming@email.com'
