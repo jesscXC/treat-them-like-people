6f4a81dbcab017cac3f43d268114eb672ea1063c
Jesse Cummins <me@jessecummins.com>
2019-09-15 19:10:50 -0400

    Set type to const rather than string

--- src/actions.js
+++ src/actions.js
@@ -2,7 +2,7 @@ import { CHANGE_SEARCHTEXT } from './constants.js';
 
 export const setSearchtext = text => {
   return {
-    type: 'CHANGE_SEARCHTEXT',
+    type: CHANGE_SEARCHTEXT,
     payload: text
   };
 };
