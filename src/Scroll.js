8211364ca0a3662ed9b16b090cc08dde3c9ebe74
Jesse Cummins <me@jessecummins.com>
2019-09-15 18:15:28 -0400

    Add scrollbar to top of page

--- src/Scroll.js
+++ src/Scroll.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 const Scroll = (props) => {
   return (
-    <div>
+    <div
+      style={{
+        overflowY: 'scroll',
+        border: '1px solid black',
+        height: '500px'
+      }}
+    >
       {props.children}
     </div>
-    );
+  );
 };
 
 export default Scroll;
